e67bb2e522b16897329896698c8a7ce8413e75b4
Дмитрий Князев <zlveresk@gmail.com>
2022-06-04 14:13:07 +0300

    1.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,31 +1,28 @@
-from turtle import title
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
-    def __str__(self):       
+    def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text= models.TextField()
-    pub_date= models.DateTimeField(auto_now_add=True)
-    group= models.ForeignKey(
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
         blank=True,
         null=True,
-        related_name='group'
-        )
-    author= models.ForeignKey(
+        related_name='group')
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
-    )
- 
+        related_name='posts')

9b7aa14abc67be34f5d9907e1519fd1eb6063621
Дмитрий Князев <zlveresk@gmail.com>
2022-06-10 22:32:19 +0300

    2.3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -14,8 +14,6 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    class Meta:
-        ordering = ["-pub_date"]
 
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -24,8 +22,13 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts')
+        related_name='posts'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts')
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ["-pub_date"]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,24 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-# Главная страница
 def index(request):
     posts = Post.objects.all()[:10]
-    # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
 
-# View-функция для страницы сообщества:
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     context = {
         'group': group,
         'posts': posts,
